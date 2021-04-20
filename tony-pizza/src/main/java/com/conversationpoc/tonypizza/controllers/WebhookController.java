59467f5668aee6542c2a7297f190e24d06768e0e
BARAKY <barak.yedaya@gmail.com>
2021-04-20 05:07:48 +0300

    c

--- tony-pizza/src/main/java/com/conversationpoc/tonypizza/controllers/WebhookController.java
+++ tony-pizza/src/main/java/com/conversationpoc/tonypizza/controllers/WebhookController.java
@@ -17,10 +17,17 @@ public class WebhookController {
 	public WebhookResponse helloWorld(@RequestBody WebhookRequest webhookRequest){
 		System.out.println("-----session is: " + webhookRequest.getSessionInfo().getSession() + "-----");
 		System.out.println("-----parameters are: " + webhookRequest.getSessionInfo().getParameters() + "-----");
-		System.out.println("-----totalAmount is: " + webhookRequest.getSessionInfo().getParameters().get("totalAmount") + "-----");
+		System.out.println("-----totalAmount is: " + webhookRequest.getSessionInfo().getParameters().get("totalAmount").toString() + "-----");
 		//-----parameters are: {drinkorder=[{original=soda, drink=soda}], pizzasize=[large], pizzatype=[napolitana], totalAmount=1, totalDeclaredAmount=1}-----
-		if(Integer.parseInt((String) webhookRequest.getSessionInfo().getParameters().get("totalAmount")) < 
-				Integer.parseInt((String)webhookRequest.getSessionInfo().getParameters().get("totalDeclaredAmount"))){
+		String[] totalAmount = (String[]) webhookRequest.getSessionInfo().getParameters().get("totalAmount");
+		double totalAmountAsDouble = 0;
+		for(int i=0; i<totalAmount.length; i++){
+			totalAmountAsDouble += (Double.parseDouble(totalAmount[i]));
+		}
+		 
+		
+		if(totalAmountAsDouble < 
+				Double.parseDouble((String)webhookRequest.getSessionInfo().getParameters().get("totalDeclaredAmount"))){
 			System.out.println("sababa");
 			webhookRequest.getSessionInfo().getParameters().replace("totalDeclaredAmount", 5);
 			WebhookResponse webhookResponse = new WebhookResponse();
