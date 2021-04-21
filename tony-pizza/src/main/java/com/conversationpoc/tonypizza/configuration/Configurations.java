7d94d21f251854dd82609e54d8b732ad7af836be
BARAKY <barak.yedaya@gmail.com>
2021-04-21 05:10:39 +0300

    c

--- tony-pizza/src/main/java/com/conversationpoc/tonypizza/configuration/Configurations.java
+++ tony-pizza/src/main/java/com/conversationpoc/tonypizza/configuration/Configurations.java
@@ -1,11 +1,13 @@
 package com.conversationpoc.tonypizza.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.conversationpoc.tonypizza.services.WebhookService;
 
 @Configuration
+@ComponentScan("com.conversationpoc.tonypizza")
 public class Configurations {
 
 	@Bean
