d01c476fa92637623b2058689bf2a4a0b6925374
BARAKY <barak.yedaya@gmail.com>
2021-04-21 05:45:46 +0300

    d

--- tony-pizza/src/main/java/com/conversationpoc/tonypizza/TonyPizzaApplication.java
+++ tony-pizza/src/main/java/com/conversationpoc/tonypizza/TonyPizzaApplication.java
@@ -2,8 +2,10 @@ package com.conversationpoc.tonypizza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.conversationpoc.tonypizza")
 public class TonyPizzaApplication {
 
 	public static void main(String[] args) {
