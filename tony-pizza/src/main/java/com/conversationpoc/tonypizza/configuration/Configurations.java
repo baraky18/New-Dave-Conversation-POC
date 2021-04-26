6f8ce6c202dbba5dfe576a48f39aaa439f2be6fa
BARAKY <barak.yedaya@gmail.com>
2021-04-26 04:50:49 +0300

    d

--- tony-pizza/src/main/java/com/conversationpoc/tonypizza/configuration/Configurations.java
+++ tony-pizza/src/main/java/com/conversationpoc/tonypizza/configuration/Configurations.java
@@ -3,6 +3,7 @@ package com.conversationpoc.tonypizza.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import com.conversationpoc.tonypizza.services.WebhookService;
 
@@ -13,5 +14,15 @@ public class Configurations {
 	@Bean
 	public WebhookService getWebhookService(){
 		return new WebhookService();
+	}
+	
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    loggingFilter.setMaxPayloadLength(64000);
+	    return loggingFilter;
 	}
 }
